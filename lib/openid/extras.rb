ae963b821c5d70c6bad48ec58a64cdf2bb926e46
Christos Trochalakis <yatiohi@ideopolis.gr>
2010-08-23 16:34:42 +0300

    Be more cautious when overloading String class

--- lib/openid/extras.rb
+++ lib/openid/extras.rb
@@ -1,11 +1,13 @@
-class String
-  def starts_with?(other)
-    head = self[0, other.length]
-    head == other
-  end
+unless "string".respond_to?(:starts_with?)
+  class String
+    def starts_with?(other)
+      head = self[0, other.length]
+      head == other
+    end
 
-  def ends_with?(other)
-    tail = self[-1 * other.length, other.length]
-    tail == other
+    def ends_with?(other)
+      tail = self[-1 * other.length, other.length]
+      tail == other
+    end
   end
 end
